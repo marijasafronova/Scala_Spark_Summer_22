8db3b78934b0c972fe8748f49c5ee50a609314e4
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-07-18 16:13:43 +0300

    Day 17 exercise

--- src/main/scala/com/github/valrcs/Day17HelloSpark.scala
+++ src/main/scala/com/github/valrcs/Day17HelloSpark.scala
@@ -9,5 +9,13 @@ object Day17HelloSpark extends App {
   //also session is a common name for the above spark object
   println(s"Session started on Spark version ${spark.version}")
 
+  val myRange = spark.range(1000).toDF("number") //create a single column dataframe (table)
+  val divisibleBy5 = myRange.where("number % 5 = 0") //so similaraities with SQL and regular Scala
+  divisibleBy5.show(10) //show first 10 rows
+
+  //TODO create range of numbers 0 to 100
+  //TODO filter into numbers divisible by 10
+  //TODO show the results
+
   spark.stop() //or .close() if you want to stop the Spark engine before the program stops running
 }
